adc0407193ef7ddb722d38329502cab8855beec5
Tomas Trnka <17759975+TomasTrnkaPLC@users.noreply.github.com>
2020-10-20 21:34:50 +0200

    Update traffick_from_google.py

--- traffick_from_google.py
+++ traffick_from_google.py
@@ -7,11 +7,10 @@ import time
 from re import search
 import random
 from random import randint
-from lackey import *
 
 
-search_query_list = ['vibracny motor', 'elektromotor' ]
-proxy_list = ['78.141.107.75:8080', '78.141.98.82:8080' ,'217.119.114.86:8080', '94.229.32.83:3128', '78.41.174.196:8081', '178.143.191.149:8080', '78.41.174.197:8081', '176.101.177.253:8080', '85.237.238.57:8080', '195.168.22.42:8080', '95.105.222.213:8080' ] 
+search_query_list = ['vibracny motor pre priemysel', 'elektromotor pre priemysel']
+proxy_list = ['78.41.174.196:8081', '178.143.191.149:8080', '78.41.174.197:8081', '176.101.177.253:8080', '85.237.238.57:8080', '195.168.22.42:8080', '95.105.222.213:8080' ] 
 
 count = 0
 
@@ -23,6 +22,7 @@ for i in range(5000):
     chrome_options.add_argument('--proxy-server=%s' % proxy_list_query)
     driver= webdriver.Chrome(chrome_options=chrome_options)
     driver = webdriver.Chrome(r"C:\chromedriver\chromedriver.exe")
+    driver.refresh()
     #open tab
     driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't') 
     # You can use (Keys.CONTROL + 't') on other OSs
@@ -36,14 +36,11 @@ for i in range(5000):
     search.send_keys(Keys.RETURN)
     assert "No results found." not in driver.page_source
     
-    #driver.find_element_by_xpath('//button[@id="introAgreeButton"]').click()
-    #driver.find_elements_by_xpath('//button[text()="Súhlasím"]').click()
-    #driver.find_element_by_xpath('//button[contains(text(), "introAgreeButton")]').click()
-    #driver.find_element_by_xpath('//button[text()="Súhlasím"]').click()
-    #driver.find_element_by_xpath("//[@id='introAgreeButton']")
-    #WebDriverWait(driver, 10).until(lambda s: s.find_element_by_id("introAgreeButton").is_displayed())
-    click("click.png")
-    time.sleep(5)
+
+    driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
+    buttons = driver.find_elements_by_xpath('//*[@id="introAgreeButton"]')
+    buttons[0].click()
+    time.sleep(randint(3,5))
     driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')  
     for i in range(randint(5,15)):
             # Load a page 
